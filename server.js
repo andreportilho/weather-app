a00768f7d3f4382a87eb03070ad1adef96d4c319
andreportilho <andre.portilhoo@gmail.com>
2018-06-17 13:36:08 -0300

    Last commit

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const token = process.env.TOKEN;
+const argv = require('yargs').agrv;
 const app = express();
 
 app.use(express.static('public'));
@@ -7,14 +12,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index', {weather: null, error: null});
 });
 
 app.post('/', function(req, res) {
-	res.render('index');
-	console.log(req.body.city);
+	let city = req.body.city;
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${token}`;
+
+	request(url, function(err, console, body) { 
+		if(err) {
+			res.render('index', {weather: null, error: 'Error, please try again'});
+		} else {
+			let weather = JSON.parse(body);
+			if(weather.main == undefined) {
+				res.render('index', {weather: null, error: 'Error, please try again'});
+			} else {
+				let weatherText = `Its ${weather.main.temp} degrees in ${weather.name}!`;
+				res.render('index', {weather: weatherText, error: null});
+			}
+		}	
+	});
 });
 
-app.listen(3000, function(){
-	console.log('Exampe app listening on port 3000');
-});
+if(process.env.PRODUCTION) {
+	const server = app.listen(process.env.PORT, () => { console.log('Exampe app listening on port %d', server.address().port);});
+} else {
+	const server = app.listen(3000, () => { console.log('Exampe app listening on port %d', server.address().port);});
+}
